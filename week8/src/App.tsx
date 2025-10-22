91da86449c11640a6ad93bd4b56398f499b58e92
KimDaegyu <136952602+KimDaegyu-Dev@users.noreply.github.com>
2025-10-22 11:32:33 +0900

    ㅂ1

--- week8/src/App.tsx
+++ week8/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Intro from "./Intro";
 import DescriptionStage from "./DescriptionStage";
 import MainStage from "./MainStage";
 
 const App: React.FC = () => {
   return (
-    <Router basename="/Web_Interaction_2025/week8">
+    <Router>
       <Routes>
         <Route path="/" element={<Intro />} />
         <Route path="/description" element={<DescriptionStage />} />
